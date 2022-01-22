b75a16bff117bbd6c206d62aedca82af7c802a6b
anandgitacc <anand.akmail@gmail.com>
2022-01-22 12:27:40 +0530

    firstgit

---
+++ src/org/gittest/Employee.java
@@ -0,0 +1,21 @@
+package org.gittest;
+
+public class Employee {
+	
+	private void empName() {
+		System.out.println("Anand");
+	}
+	
+	private void empMobile() {
+		System.out.println("8939278836");
+	}
+	
+	public static void main(String[] args) {
+		
+		Employee e = new Employee();
+		e.empMobile();
+		e.empName();
+	}
+	
+
+}
